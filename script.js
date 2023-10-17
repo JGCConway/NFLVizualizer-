2af57ae10e71fa174a9b9676ebd1eb0fc6f2d0f6
JGCConway <justinconway022@gmail.com>
2023-10-16 22:09:59 -0500

    Recolor - Resize - Refresh

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let tabs = document.querySelectorAll(".tabs h3");
+let tabs = document.querySelectorAll(".Container-Tabs h3");
 let tabContents = document.querySelectorAll(".tab-content div");
 tabs.forEach((tab, index) => {
   tab.addEventListener("click", () => {
